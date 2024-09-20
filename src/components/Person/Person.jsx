9b6a57813ba58031c5afa6cc3d456f1e05c07544
Влад Теут <vladteut@Mac.localdomain>
2024-09-20 16:07:37 +0300

    taskReactPersoFix2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,11 @@
 export const Person = ({ person }) => {
-  const getPartnerText = () => {
-    if (person.isMarried) {
-      const partnerType = person.sex === 'm' ? 'wife' : 'husband';
+  const { name, age, isMarried, sex, partnerName } = person;
 
-      return `${person.partnerName} is my ${partnerType}`;
+  const getPartnerText = () => {
+    if (isMarried) {
+      const partnerType = sex === 'm' ? 'wife' : 'husband';
+
+      return `${partnerName} is my ${partnerType}`;
     }
 
     return 'I am not married';
@@ -11,8 +13,8 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">{getPartnerText()}</p>
     </section>
   );
